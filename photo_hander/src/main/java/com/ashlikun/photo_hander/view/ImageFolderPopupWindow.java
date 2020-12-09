0ade8430720fb5b47078fd4fceba9d54c53fb6cd
爱生活、李坤 <496546144@qq.com>
2020-12-09 15:35:58 +0800

    新增启动方式的回调

--- photo_hander/src/main/java/com/ashlikun/photo_hander/view/ImageFolderPopupWindow.java
+++ photo_hander/src/main/java/com/ashlikun/photo_hander/view/ImageFolderPopupWindow.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -36,6 +38,7 @@ public class ImageFolderPopupWindow extends PopupWindow {
      * 数据来源
      */
     private MediaHandler mediaHandler;
+    View backgoundView;
 
     public ImageFolderPopupWindow(Activity activity, MediaHandler mediaHandler) {
         this.mActivity = activity;
@@ -58,14 +61,14 @@ public class ImageFolderPopupWindow extends PopupWindow {
     @Override
     public void dismiss() {
         super.dismiss();
-        setLightMode(false);
+        handleBackgroundAlpha(false);
     }
 
 
     @Override
     public void showAsDropDown(View anchor, int xoff, int yoff, int gravity) {
         super.showAsDropDown(anchor, xoff, yoff, gravity);
-        setLightMode(true);
+        handleBackgroundAlpha(true);
     }
 
     /**
@@ -79,6 +82,45 @@ public class ImageFolderPopupWindow extends PopupWindow {
             layoutParams.alpha = 1f;
         }
         mActivity.getWindow().setAttributes(layoutParams);
+    }
+
+    /**
+     * 处理背景变暗
+     *
+     * @param isShow 是否是弹出时候调用的
+     */
+    private void handleBackgroundAlpha(boolean isShow) {
+        Activity activity = mActivity;
+        if (activity != null) {
+            View decorView = activity.getWindow().getDecorView();
+            FrameLayout rootView = (FrameLayout) decorView.findViewById(android.R.id.content);
+            if (isShow) {
+                backgoundView = new View(activity);
+                //设置宽高为全屏
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
+                        (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                backgoundView.setLayoutParams(layoutParams);
+                //设置背景颜色为黑色，加上透明度，就会有半透明的黑色蒙版效果
+                backgoundView.setBackgroundColor(0xff000000);
+                //1.0f 不透明/0.0f 透明
+                backgoundView.setAlpha(0.7f);
+                rootView.addView(backgoundView);
+            } else {
+                rootView.removeView(backgoundView);
+            }
+//                WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+//                if (isShow && mWindowAlphaOrgin == DEFAULT_WINDOW_ALPHA_ORGIN) {
+//                    mWindowAlphaOrgin = lp.alpha;
+//                }
+////                lp.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+//                if (isShow) {
+//                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+//                } else {
+//                    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+//                }
+//                lp.alpha = isShow ? mBackgroundAlpha : mWindowAlphaOrgin;
+//                activity.getWindow().setAttributes(lp);
+        }
     }
 
     /**
